d28e42ed75fe7280bc3708b56dab581ef078140e
SYED175 <47055652+SYED175@users.noreply.github.com>
2021-08-18 12:48:34 +0530

    Add files via upload

---
+++ springboot-patient-registration/model/Employee.java
@@ -0,0 +1,62 @@
+package net.javaguides.springboot.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Patients")
+public class Employee {
+	
+	@Id
+	@Column(name = "AadharNumber")
+	private long AadharNumber;
+	
+	@Column(name = "first_name")
+	private String firstName;
+	
+	@Column(name = "last_name")
+	private String lastName;
+	
+	@Column(name = "email")
+	private String email;
+	
+	@Column(name = "phoneNumber")
+	private String phoneNumber;
+	
+	public long getAadharNumber() {
+		return AadharNumber;
+	}
+	public void setAadharNumber(long aadharNumber) {
+		AadharNumber = aadharNumber;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
